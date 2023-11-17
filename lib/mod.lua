60cd38cc5d902d9af3a2aadd8757fc1e867d45b4
Dan Stroud <dstroud@mac.com>
2023-11-17 14:58:20 -0600

    wip in live vport changes

--- lib/mod.lua
+++ lib/mod.lua
@@ -6,9 +6,20 @@ local port = 10111
 -- local midi_over_osc_id = -2    -- populate from global then kill globals
 -- local midi_over_osc_vport = 18
 
+-- debug function. remove!
+function vp(idx)
+  local vp = midi.vports[idx]
+  print("id", idx)
+  print("name", vp.name)
+  print("connected", vp.connected)
+  print("event", type(vp.event))
+  print("device", type(vp.event))  
+end  
+
 local my_midi = {
-  name="MIDI-OVER-OSC",
-  connected=true,
+  name = "MIDI-OVER-OSC",
+  connected = true,
+  -- device = midi_over_osc_id  -- todo: test. Not present for nbout FWIW. Is set when connecting 
 }
 
 function my_midi:send(data) end
@@ -30,7 +41,7 @@ function my_midi:channel_pressure(val, ch) end
 function my_midi:program_change(val, ch) end
 function my_midi:stop()	end
 function my_midi:continue()	end
-function my_midi:clock() end
+function my_midi:clock() end -- likely would need redefining Midi.update_connected_state
 
 local fake_midi = {
   real_midi = midi,
@@ -43,46 +54,33 @@ setmetatable(fake_midi, meta_fake_midi)
 
 meta_fake_midi.__index = function(t, key)
 
-  -- print("meta_fake_midi touched")  -- turn this on to see the issue sending event :/
-
-  
 	if key == 'vports' then
 		local ret = {}
+		-- print("meta vports accessed")
 		for _, v in ipairs(t.real_midi.vports) do
-			table.insert(ret, v)
-
-		-- OPTIONAL method of immediately updating midi tables
-		-- does not require mod re-init
-		-- 	if v.name == "MIDI-OVER-OSC" then
-			 -- print("setting midi-over-osc vport  to " .. _)
-			 -- midi_over_osc_vport = _
-
-			 -- print("setting midi.devices port to " .. _)
-			 -- fake_midi.devices[midi_over_osc_id].port = _
-		-- 	end
-	  
-		end
-
-    -- option 1
-		-- -- This is where we insert vport 17 every time
-		-- -- why does this work but if we insert into a specific index <=16 it doesn't??
-		-- print("#midi.vports = " .. #ret)
-		-- table.insert(ret, my_midi)
-		-- print("#midi.vports = " .. #ret)
-
-		
-
-  --   option 2: seems to work but not sure if we should always set (option 3)
-		-- only insert if vport hasn't been set?
-		if midi_over_osc_vport > 16 then
-		  -- table.insert(ret, my_midi)  -- insert
-		  ret[midi_over_osc_vport] = my_midi  -- or set explicitly. 
-	  end
-		
-		
-    -- option 3: set my_midi explicity every time. works for 17 but not for 1-16..
-		  -- ret[midi_over_osc_vport] = my_midi  -- or set explicitly. 
 		  
+		  -- -- this sets the port correctly when changed via Devices menu.
+		  -- -- todo: won't be recognized until script restart
+		  -- -- after relaunching script sometimes it gets confused, e.g.:
+		  -- -- tab.print(midi.vports[7].connected) == true
+		  -- -- tab.print(midi.real_midi.vports[7].connected) == false
+		  -- if v.name == "MIDI-OVER-OSC" then
+			 -- print("setting vport/device.port to " .. _)
+			 -- midi_over_osc_vport = _
+			 -- midi.devices[midi_over_osc_id].port = midi_over_osc_vport
+		  -- end
+		  
+			table.insert(ret, v)
+		  
+		end
+			
+			
+    -- print("setting vports[" .. midi_over_osc_vport .. "] to my_midi")
+    -- print("my_midi.name = " .. (my_midi.name or "nil"))
+    -- print("my_midi.connected = " .. (tostring(my_midi.connected or nil)))
+		ret[midi_over_osc_vport] = my_midi
+
+
 		-- print("post-reading from ret")
 		-- print(ret[midi_over_osc_vport].name)
 		
@@ -96,8 +94,10 @@ meta_fake_midi.__index = function(t, key)
 		end
 		ret[midi_over_osc_id] = {
 			name="MIDI-OVER-OSC",
-			port=midi_over_osc_vport, -- todo: not updated when vport changes (until mod re-init)
+			port=midi_over_osc_vport, -- todo: doesn't reflext vport changes until mod re-init
 			id=midi_over_osc_id,
+			-- ** todo: switching vport 17 to 5 does set vport name but vport.connected stays on port 17. Probably because it's pulling from here.
+			-- looks as if event placed here can also fire, fwiw (untested)
 		}
 		return ret
 	end
@@ -108,26 +108,39 @@ meta_fake_midi.__index = function(t, key)
 				idx = 1
 			end
 			
-		-- 	print("idx = " .. idx, "midi_over_osc_vport = " .. (midi_over_osc_vport or "nil"))
+  --     -- shitty at the moment but the idea is this:
+  --     -- if idx > 16 and idx == midi_over_osc_vport then return my_midi
+  --     -- else return t.real_midi.connect(idx)
+  --     -- needs to be tested with nb port 17
+  --     -- probably interacts with vport flow which is wip
 			
-			if idx == midi_over_osc_vport then  -- todo p0
-				return my_midi
-			end
-			
-			if idx <= 16 then
-				if t.real_midi.vports[idx].name == "MIDI-OVER-OSC" then
-					print("Connecting to MIDI-OVER-OSC vport " .. idx)
-				end
-				return t.real_midi.connect(idx)
-			end
-
-      -- print("this never runs unless > 16 because return breaks it")
-      
-  --     -- og which works but only on port 17 I suspect
-		-- 	if idx == #t.real_midi.vports + 1 then  -- todo p0
-		-- 		return my_midi
+		-- 	if idx <= 16 then
+		-- 	  if idx == midi_over_osc_vport then
+		-- 	    print("Connecting to MIDI-OVER-OSC vport <= 16: " .. idx)
+		--     end
+		-- 	  return t.real_midi.connect(idx)
 		-- 	end
 
+  --     if idx > 16 then
+  --       if idx ~= midi_over_osc_vport then
+  --         print("idx > 16 but not MIDI-OVER-OSC (nbout?):" .. idx)
+		-- 	    return t.real_midi.connect(idx) --??
+  --       else
+		-- 	    print("Connecting to MIDI-OVER-OSC vport > 16: " .. idx)
+		-- 	    return my_midi
+		--     end
+  --     end
+
+
+      -- in conjunction with vport option 3 above
+      if idx == midi_over_osc_vport then
+        print("connecting to my_midi")
+        return my_midi
+      else
+        print("passing through midi.connect")
+        return t.real_midi.connect(idx)
+      end
+ 
 			return nil
 		end
 	end
@@ -188,6 +201,7 @@ mod.hook.register("script_pre_init", "midi-over-osc pre init", function()
     old_osc_event = osc.event
 
   	function osc.event(path, args, from)
+  	  print("OSC received")
   	  if path == "/midi-over-osc" then
 
         -- touching midi table now goes through a metatable which adds quite a bit of overhead. Need to maybe store the event function elsewhere to avoid this.
